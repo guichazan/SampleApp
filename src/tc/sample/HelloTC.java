78fbc9c5c81a8e1b937efec39676f6bafd560c4b
Bruno Muniz <bruno@softsite.com.br>
2017-01-20 15:43:26 -0300

    Added some comments about the source code

--- src/tc/sample/HelloTC.java
+++ src/tc/sample/HelloTC.java
@@ -24,12 +24,27 @@ import totalcross.ui.image.Image;
 import totalcross.util.Date;
 import totalcross.util.InvalidDateException;
 
+/**
+ * Simple example to help you create your own app with TotalCross.
+ * 
+ * Feel free to modify this code.
+ * 
+ * If you need more help, reach us at www.totalcross.com
+ * 
+ * You can find the full explanation of this sample at
+ * 
+ * http://www.totalcross.com/documentation/getting_started.html
+ * 
+ *
+ */
 public class HelloTC extends MainWindow {
 	private Edit edName, edBorn, edPhone;
 	private Button btInsert, btClear, btDial;
 	private Connection dbcon;
 
-	private final int componentH = fmH * 2;
+	// fmH is the font size. We will set the components to have twice the size
+	// of font.
+	private final int COMPONENT_H = fmH * 2;
 
 	private final int FLAT_EDGE_MARGIN = (int) (Math.min(Settings.screenHeight,
 			Settings.screenWidth) * 0.10);
@@ -52,7 +67,8 @@ public class HelloTC extends MainWindow {
 
 		try {
 
-			// add the logo at top
+			// add the logo at top. Copy your logo to img folder and try to
+			// change it.
 			ImageControl ic = new ImageControl(new Image("img/logo.png"));
 			ic.scaleToFit = ic.centerImage = true;
 			add(ic, LEFT, PARENTSIZE + 12, FILL, PARENTSIZE + 15);
@@ -65,9 +81,12 @@ public class HelloTC extends MainWindow {
 
 			// regular font is not bold
 			add(new Label("Name: "), LEFT + FLAT_EDGE_MARGIN, AFTER + 100, FILL
-					- FLAT_EDGE_MARGIN, componentH);
+					- FLAT_EDGE_MARGIN, COMPONENT_H);
 			add(edName = new Edit(), SAME, AFTER, SAME, SAME);
 
+			// Position of components are relative to screen and other
+			// components. You can align components to TOP or BOTTOM, LEFT and
+			// RIGHT and AFTER other components
 			add(new Label("Born Date: "), SAME, AFTER, PREFERRED, SAME);
 			add(edBorn = new Edit(), SAME, AFTER, FILL - FLAT_EDGE_MARGIN, SAME);
 			edBorn.setMode(Edit.DATE);
@@ -82,19 +101,24 @@ public class HelloTC extends MainWindow {
 
 			add(edPhone = new Edit(), LEFT + FLAT_EDGE_MARGIN, SAME, FIT
 					- FLAT_EDGE_MARGIN, SAME);
+
+			// There are a set of predefined keyboards that you can use.
 			edPhone.setKeyboard(Edit.KBD_NUMERIC);
 
 			Spacer sp = new Spacer(0, 0);
-			add(sp, CENTER, BOTTOM - 300, PARENTSIZE + 10, componentH);
+			add(sp, CENTER, BOTTOM - 300, PARENTSIZE + 10, COMPONENT_H);
 			add(btInsert = new Button("Insert"), BEFORE, SAME, PARENTSIZE + 40,
-					componentH, sp);
+					COMPONENT_H, sp);
 			add(btClear = new Button("Clear"), AFTER, SAME, PARENTSIZE + 40,
-					componentH, sp);
+					COMPONENT_H, sp);
 
 			// setting colors for buttons...
 			btInsert.setBackForeColors(0x049CEE, Color.WHITE);
 			btClear.setBackColor(Color.PINK);
 
+			// You can also create behaviors to specific OS. For Example:
+			// Settings.isIOS() returns a boolean indicating if the OS is an
+			// iOS..
 			if (Settings.onJavaSE || Settings.platform.equals(Settings.WIN32))
 				add(new Label("Press F11 on date/number to open keypad"),
 						CENTER, BOTTOM);
@@ -130,10 +154,13 @@ public class HelloTC extends MainWindow {
 	}
 
 	private void doInsert() throws SQLException, InvalidDateException {
+
 		if (edName.getLength() == 0 || edBorn.getLength() == 0
 				|| edPhone.getLength() == 0)
 			showToast("Please fill all fields!");
 		else {
+
+			// simple example of how you can insert data into SQLite..
 			String name = edName.getText();
 			Date born = new Date(edBorn.getText());
 			String phone = edPhone.getText();
