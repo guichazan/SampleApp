3a474dde3cfc1d0c1120e3ec81faf015357aae7c
Jefferson Quesado <jeff.quesado@gmail.com>
2017-05-02 16:21:53 -0300

    Mirrorring wiki's example

--- src/main/java/com/totalcross/sample/HelloTcApplication.java
+++ src/main/java/com/totalcross/sample/HelloTcApplication.java
@@ -5,8 +5,10 @@ import totalcross.TotalCrossApplication;
 import com.totalcross.sample.ui.HelloTC;
 
 public class HelloTcApplication {
-    public static void main(String[] args) {
-	TotalCrossApplication.run(HelloTC.class, 
-		new String[]{"/scr","android","/fontsize","20", "/fingertouch"});
-    }
+	public static void main(String[] args) {
+		TotalCrossApplication.run(HelloTC.class, "/r", "ACTIVATION_KEY",
+				"/scr", "android",
+				"/fontsize", "20",
+				"/fingertouch");
+	}
 }
