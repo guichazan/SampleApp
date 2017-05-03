af5b4fbf22220fbc08d884263aa1b62bd280d077
Jefferson Quesado <jeff.quesado@gmail.com>
2017-05-02 22:44:21 -0300

    Helloing with lambda and buttons?

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,13 +1,17 @@
 package hello;
 
 import totalcross.ui.MainWindow;
-import totalcross.ui.Label;
+import totalcross.ui.Toast;
+import totalcross.ui.Button;
 
 public class HelloWorld extends MainWindow {
-
 	@Override
     public void initUI() {
+		Button greetings = new Button("Greetings");
         Greeter greeter = new Greeter();
-        add(new Label(greeter.sayHello()), CENTER, CENTER);
+        add(greetings, CENTER, CENTER);
+        greetings.addPressListener((e) -> {
+        	Toast.show(greeter.sayHello(), 1000);
+        });
     }
 }
