bc73cc77d5c54a4e317d5c7a8eff819d3b44ecbe
Jefferson Quesado <jeff.quesado@gmail.com>
2017-05-02 22:28:59 -0300

    Forgot to override stuff

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,6 +5,7 @@ import totalcross.ui.Label;
 
 public class HelloWorld extends MainWindow {
 
+	@Override
     public void initUI() {
         Greeter greeter = new Greeter();
         add(new Label(greeter.sayHello()), CENTER, CENTER);
